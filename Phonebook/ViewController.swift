e7a284bd7eeff1b27618a8944fd69499f1032516
Ricardo Pereira <m@ricardopereira.eu>
2022-01-22 11:31:21 +0000

    CNContactStore experiments.

--- Phonebook/ViewController.swift
+++ Phonebook/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import Contacts
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Phonebook"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshButtonTapped))
     }
 
+    @objc func refreshButtonTapped() {
+        // Because CNContactStore fetch methods perform I/O, it’s recommended that you avoid using the main thread to execute fetches.
+        let authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
+        switch authorizationStatus {
+        case .authorized:
+            fetchContacts()
+        case .denied:
+            print("The user explicitly denied access to contact data for the application.")
+        case .notDetermined:
+            print("CNContactStore().requestAccess")
+            CNContactStore().requestAccess(for: .contacts) { granted, error in
+                print("Granted:", granted)
+                print("Error:", error ?? "none")
+                if granted {
+                    self.fetchContacts()
+                }
+            }
+        case .restricted:
+            print("The application is not authorized to access contact data.")
+        @unknown default:
+            fatalError("CNAuthorizationStatus '\(authorizationStatus)' not implemented")
+        }
+    }
+
+    func fetchContacts() {
+        // Fetch
+        let contactStore = CNContactStore()
+        var contacts = [CNContact]()
+        let fetchRequest = CNContactFetchRequest(
+            keysToFetch: [
+                CNContactVCardSerialization.descriptorForRequiredKeys()
+            ]
+        )
+        do {
+            try contactStore.enumerateContacts(
+                with: fetchRequest,
+                usingBlock: { contact, _ in
+                    contacts.append(contact)
+                }
+            )
+            for contact in contacts {
+                print(contact.identifier, contact.givenName, contact.familyName, contact.phoneNumbers.map({ $0.value.stringValue }))
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
 
 }
-
